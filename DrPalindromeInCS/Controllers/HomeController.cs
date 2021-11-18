b8ced368c866dcc474e3d44835b16262269e9415
Jory <46983170+JorySmith@users.noreply.github.com>
2021-11-18 12:14:06 -0600

    Updated palindrome page and index page
    Created model class Palindrome with several methods
    Created C# code to take in users input string, reverse it, remove special characters or spaces, and check if result still matches spelling of the input string.

--- DrPalindromeInCS/Controllers/HomeController.cs
+++ DrPalindromeInCS/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace DrPalindromeInCS.Controllers
@@ -26,6 +27,51 @@ namespace DrPalindromeInCS.Controllers
         public IActionResult Privacy()
         {
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult Reverse()
+        {
+            Palindrome model = new();
+
+            return View(model);
+        }
+
+        [HttpPost]
+        // Ensure proper form is submitted/posted
+        [ValidateAntiForgeryToken]
+        // Create in IAR for Reverse Post and supply the Palindrome palindrome parameter
+        public IActionResult Reverse(Palindrome palindrome)
+        {
+            string inputWord = palindrome.InputWord;
+            string revWord = "";
+
+            // Loop through input word from the end going forward, pushing each letter to revWord
+            for (int i = inputWord.Length - 1; i >= 0; i--)
+            {
+                revWord += inputWord[i];
+            }
+
+            // Assign revWord to model palindrome.RevWord 
+            palindrome.RevWord = revWord;
+
+            // Use Regex.Replace() to remove special characters and spaces from revWord and inputWord for proper comparison
+            revWord = Regex.Replace(revWord.ToLower(), "[^a-zA-Z0-9]+", "");
+            inputWord = Regex.Replace(inputWord.ToLower(), "[^a-zA-Z0-9]+", "");
+
+            // Check if user's inputWord == revWord, update model palindrome class (isPalindrome bool and Message) with results 
+            if (revWord == inputWord)
+            {
+                palindrome.IsPalindrome = true;
+                palindrome.Message = $"Success! --{palindrome.InputWord}-- is a palindrome";
+            }
+            else
+            {
+                palindrome.IsPalindrome = false;
+                palindrome.Message = $"I'm sorry, --{palindrome.InputWord}-- is not a palindrome";
+            }
+
+            return View(palindrome);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
